22b6a311dd5b7d1d1446642b9297c9acf6371ef3
Tasia <mawufantasy@gmail.com>
2026-08-14 13:29:58 +0000

    We are finished

--- lms_backend/src/main/java/lms/lms/LmsApplication.java
+++ lms_backend/src/main/java/lms/lms/LmsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import lms.lms.members.repository.MemberRepository;
+import lms.lms.members.entity.Role;
 
 @SpringBootApplication
 public class LmsApplication {
