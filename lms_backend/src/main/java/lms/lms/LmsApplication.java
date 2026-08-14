d43423340f10db1351d48b457d01b3797eaf5175
Tasia <mawufantasy@gmail.com>
2026-08-14 13:01:05 +0000

    All done

--- lms_backend/src/main/java/lms/lms/LmsApplication.java
+++ lms_backend/src/main/java/lms/lms/LmsApplication.java
@@ -1,13 +1,25 @@
 package lms.lms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import lms.lms.members.repository.MemberRepository;
 
 @SpringBootApplication
 public class LmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LmsApplication.class, args);
-	}
+    @Bean
+    CommandLineRunner makeAdmin(MemberRepository memberRepository) {
+        return args -> {
+            memberRepository.findByEmail("mawufantasy@gmail.com").ifPresent(member -> {
+				member.setRole(Role.ADMIN);
+                memberRepository.save(member);
+            });
+        };
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(LmsApplication.class, args);
+    }
 }
